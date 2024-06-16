420b4b2b83744a79d1f13c10a84e0128fd9d298f
Franklin M <franklinmuriithi431@gmail.com>
2024-06-16 07:21:54 +0300

    Mrng 1

--- index.js
+++ index.js
@@ -30,7 +30,13 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors(
+    {
+        origin:"https://dev-blog1-frontend.onrender.com/",
+        methods:["GET", "POST"],
+        credentials:true
+    }
+    ))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
