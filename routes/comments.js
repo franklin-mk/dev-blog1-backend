2c8523af7606c256c6c001c9e2f7813aaedf5441
Franklin M <franklinmuriithi431@gmail.com>
2024-06-17 12:39:06 +0300

    M1

--- routes/comments.js
+++ routes/comments.js
@@ -50,7 +50,7 @@ router.delete("/:id", verifyToken,  async (req,res)=>{
 
 
 //GET POST COMMENTS
-router.get("/post/:postId",async (req,res)=>{
+router.get("/post/:postId", verifyToken, async (req,res)=>{
     try{
         const comments=await Comment.find({postId:req.params.postId})
         res.status(200).json(comments)
